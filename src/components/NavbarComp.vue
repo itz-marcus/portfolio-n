9f0278daae2818c2e988210813ddd64becdd721e
itz_marcus <marcusadonis5@gmail.com>
2024-12-12 07:50:45 +0200

    Fix

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -5,7 +5,7 @@
           <router-link to="/about">About</router-link>
           <router-link id="nav3" to="/resume">Edu. & Work</router-link>
           <router-link to="/projects">Projects</router-link>
-          <router-link to="/case-studies">Cases</router-link>
+          <!-- <router-link to="/case-studies">Cases</router-link> -->
           <router-link id="nav6" to="/contact">Contact</router-link>
     </nav> 
   </div>
